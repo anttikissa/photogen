545ea14f80808d44b9584aaa38a88d408037d75e
Antti Sykäri <antti.sykari@gmail.com>
2013-08-21 15:26:32 +0300

    Initial photos.json generator script in bash.

--- gen.sh
+++ gen.sh
@@ -1,12 +1,42 @@
 #!/bin/bash
 
-if ! which exiv2 2> /dev/null
+if ! which exiv2 2>&1 >/dev/null
 then
 	echo 'Please install exiv2 before proceeding.'
 	exit
 fi
 
+TARGET=./photos.json
+
+echo -n > $TARGET
+
+function output {
+	echo $@ >> $TARGET
+}
+
+output '['
+
+first=1
+photos=0
+
 for x in *.jpg
 do
-	exiv2 -pt $x
+#	exiv2 -pt $x
+	if (( $photos ))
+	then
+		output ","
+	fi
+
+	output -en '\t{ file: "'$x'" }'
+
+#	if (( $first ))
+#		then echo
+#		else echo ','
+#	fi
+	
+	photos=$(($photos + 1))
 done
+
+output -e '\n]'
+
+echo Generated photos.json with $photos photos.
